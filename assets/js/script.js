48e0c52f6eee87534bbbb46895380eb3d04c76fb
UllasZIU2 <md.zihadul.islam@g.bracu.ac.bd>
2025-05-15 02:22:54 +0600

    seo update

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,3 +87,25 @@ $(function() {
     });
     
 });
+// Initialize tooltips for certification links
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+    
+    // Smooth scrolling for all links
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+    
+    // Initialize progress bars
+    $('.progress .progress-bar').progressbar({
+        display_text: 'fill'
+    });
+});
